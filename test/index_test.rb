ba0f5e0038bcf58574da8459c8f6bd6499e3db29
harrisbaird <mydancake@gmail.com>
2016-08-14 16:33:23 +0100

    Run tests on travis

--- test/index_test.rb
+++ test/index_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class HomeTest < Minitest::Capybara::Test
   def test_index_redirection
     visit '/'
-    assert_equal '/posts', current_path
+    assert_equal '/posts/index', current_path
   end
 end
